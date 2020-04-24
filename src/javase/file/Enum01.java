75d8128783dad83bda3fd653bb5d2b6ce30db09f
WildGuGu <1273036191.qq.com>
2020-04-24 17:50:58 +0800

    java -v1.1.8

---
+++ src/javase/file/Enum01.java
@@ -0,0 +1,10 @@
+package javase.file;
+
+public class Enum01 {
+    public static void main(String[] args) {
+        System.out.println(Season.SPRING);
+    }
+}
+enum Season{
+    SPRING,SUMMER,AUTUMN,WINTER
+}
