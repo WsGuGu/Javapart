036656e6ecf1a8bae706cc3979c5d70f09b19c06
WildGuGu <1273036191.qq.com>
2020-04-28 19:35:41 +0800

    java -v1.2.0

---
+++ src/javase/collection/Generic01.java
@@ -0,0 +1,22 @@
+package javase.collection;
+
+public class Generic01 {
+    public static void main(String[] args) {
+        MyCollection<String> mc=new MyCollection<>();
+        mc.set("whj",0);
+        String a=mc.get(0);
+        System.out.println(a);
+
+
+
+    }
+}
+class MyCollection<E>{
+    Object[] objs=new Object[5];
+    public void set(E obj,int index){
+        objs[index]=obj;
+    }
+    public E get(int index){
+        return (E)objs[index];
+    }
+}
---
+++ src/javase/collection/Generic02.java
@@ -0,0 +1,11 @@
+package javase.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generic02 {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+
+    }
+}
