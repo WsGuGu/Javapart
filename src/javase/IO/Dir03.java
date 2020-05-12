545270cb4f0daacef1f2ee315c806c9eab9b196b
WildGuGu <1273036191.qq.com>
2020-05-12 18:20:31 +0800

    java -v1.2.4

---
+++ src/javase/IO/Dir03.java
@@ -0,0 +1,24 @@
+package javase.IO;
+
+import java.io.File;
+
+public class Dir03 {
+    public static void main(String[] args) {
+        File dir =new File("D:/programming/programming_workingspace/exercise/java/src/javase/IO");
+        printName(dir,0);
+    }
+    public static void  printName(File src,int deep){
+        for (int i=0;i<deep;i++){
+            System.out.print("-");
+        }
+        System.out.println(src.getName());
+        if (null==src ||!src.exists()){
+            return;
+        }else if(src.isDirectory()) {
+            for (File s:src.listFiles()){
+                printName(s,deep+1);
+            }
+        }
+
+    }
+}
