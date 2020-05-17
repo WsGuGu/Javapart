bf4d2c8ca531435bbb1a14e0cdf578c4fa43d2ac
WildGuGu <1273036191.qq.com>
2020-05-17 21:09:10 +0800

    java -v1.2.4

---
+++ src/javase/IO/Io01.java
@@ -0,0 +1,29 @@
+package javase.IO;
+
+import java.io.*;
+
+public class Io01 {
+    public static void main(String[] args) {
+        File src=new File("src/javase/IO/test01.txt");
+        InputStream is=null;
+        try {
+            is=new FileInputStream(src);
+            int temp=0;
+            while ((temp=is.read())!=-1){
+                System.out.print((char) temp);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (is!=null){
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
