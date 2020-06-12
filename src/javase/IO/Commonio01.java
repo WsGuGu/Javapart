abb12e85dcf608aa21ad568f8e27f1a5f9e00e93
WildGuGu <1273036191.qq.com>
2020-06-12 17:59:32 +0800

    java -v1.3.4

---
+++ src/javase/IO/Commonio01.java
@@ -0,0 +1,16 @@
+package javase.IO;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class Commonio01 {
+    public static void main(String[] args) {
+        //文件大小
+        long len= FileUtils.sizeOf(new File("src/javase/IO/test01.txt"));
+        System.out.println(len);
+        //目录大小
+        len=FileUtils.sizeOf(new File("src/javase/IO"));
+        System.out.println(len);
+    }
+}
