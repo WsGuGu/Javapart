f9232126c9c87a8b3e208a48380cf91ee5235c69
WildGuGu <1273036191.qq.com>
2020-06-18 17:39:52 +0800

    java -v1.3.8

---
+++ src/javase/thread/LamdaThread01.java
@@ -0,0 +1,21 @@
+package javase.thread;
+
+public class LamdaThread01 {
+    //静态内部类
+    static class Test implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 20; i++) {
+                System.out.println("一边听歌");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Test()).start();
+        //lambda方式
+        new Thread(()->{ for (int i = 0; i < 20; i++)
+        {
+            System.out.println("一边跳舞"); }}).start();
+    }
+}
