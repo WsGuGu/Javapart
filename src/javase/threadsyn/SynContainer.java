5dc9893dbf1e8120f229ad90df09a148c4e2ce10
WildGuGu <1273036191.qq.com>
2020-06-21 12:10:04 +0800

    java -v1.4.1

---
+++ src/javase/threadsyn/SynContainer.java
@@ -0,0 +1,19 @@
+package javase.threadsyn;
+
+//vector也可以
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**线程安全操纵容器*/
+public class SynContainer {
+    public static void main(String[] args) throws InterruptedException {
+        CopyOnWriteArrayList<String> list=new CopyOnWriteArrayList<>();
+        for (int i=0;i<10000;i++){
+            //同步块
+            new Thread(()->{
+                    list.add(Thread.currentThread().getName());
+            }).start();
+        }
+        Thread.sleep(10000);
+        System.out.println(list.size());
+    }
+}
