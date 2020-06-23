84649340c5418ec17019574c6869d3b0333383e7
WildGuGu <1273036191.qq.com>
2020-06-23 19:10:17 +0800

    java -v1.4.4

---
+++ src/javase/cooperation/LockTest01.java
@@ -0,0 +1,22 @@
+package javase.cooperation;
+/**可重入锁，锁可以延续使用*/
+public class LockTest01 {
+    public void test(){
+        synchronized (this){
+            while (true){
+                synchronized (this){
+                    System.out.println("ReentrantLock");
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new LockTest01().test();
+    }
+}
---
+++ src/javase/cooperation/VolatileTest01.java
@@ -0,0 +1,14 @@
+package javase.cooperation;
+/**volatile用来保证数据的可见性*/
+public class VolatileTest01 {
+    private volatile static int num=0;
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(()->{
+            while (num==0){
+
+            }
+        }).start();
+        Thread.sleep(1000);
+        num=1;
+    }
+}
