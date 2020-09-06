0909c06220f8152c19c089b9fc0909d3e65d0fe0
WsGuGu <wildgugu@qq.com>
2020-09-06 17:13:30 +0800

    java -v1.6.6

---
+++ src/javase/classload/Demo02.java
@@ -0,0 +1,12 @@
+package javase.classload;
+
+/**
+ * @email:wildgugu@qq.com
+ * @Date:2020/9/6--17:02
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println(ClassLoader.getSystemClassLoader());
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
