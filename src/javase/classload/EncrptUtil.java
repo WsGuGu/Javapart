a698f00baa4981bf7c8933484cda0d6fbd3f746b
WsGuGu <wildgugu@qq.com>
2020-09-08 17:12:44 +0800

    java -v1.6.8

---
+++ src/javase/classload/EncrptUtil.java
@@ -0,0 +1,42 @@
+package javase.classload;
+
+import java.io.*;
+
+/**
+ * @email:wildgugu@qq.com
+ * @Date:2020/9/8--16:47
+ */
+public class EncrptUtil {
+    public static void main(String[] args) {
+        encrpt("xxxxx.class","xxxxx.class");
+    }
+    public static void encrpt(String src,String dest){
+        FileInputStream fis=null;
+        FileOutputStream fos=null;
+        try {
+            fis=new FileInputStream(src);
+            fos=new FileOutputStream(dest);
+            int temp=-1;
+            while ((temp=fis.read())!=-1){
+                fos.write(temp^0xff);//取反操作
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (fis!=null){
+                    fis.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            try {
+                if (fos!=null){
+                    fos.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/javase/classload/FileSystemClassLoader.java
+++ src/javase/classload/FileSystemClassLoader.java
@@ -1,5 +1,7 @@
 package javase.classload;
 
+import java.io.*;
+
 /**
  * @email:wildgugu@qq.com
  * @Date:2020/9/7--17:45
@@ -10,7 +12,31 @@ public class FileSystemClassLoader extends ClassLoader{
     public FileSystemClassLoader(String rootDir) {
         this.rootDir = rootDir;
     }
-
+    private byte[] getClassData(String classname){
+        String path=rootDir+"/"+classname.replace('.','/')+".class";
+        InputStream is=null;
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        try {
+             is=new FileInputStream(path);
+             byte[] buffer=new byte[1024];
+             int temp=0;
+             while ((temp=is.read(buffer))!=-1){
+                 baos.write(buffer,0,temp);
+             }
+             return baos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }finally {
+            if (is!=null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         Class<?> c=findLoadedClass(name);
@@ -20,7 +46,24 @@ public class FileSystemClassLoader extends ClassLoader{
         }
         else {
             ClassLoader parent=this.getParent();
-            return c;//删掉重写
+            try {
+                parent.loadClass(name);//委派给父类加载
+            } catch (Exception e) {
+                //
+            }
+            if(c!=null){
+                return c;
+            }
+            else {
+                byte[] classData=getClassData(name);
+                if (classData==null){
+                    throw new ClassNotFoundException();
+                }
+                else {
+                    c=defineClass(name,classData,0,classData.length);
+                }
+            }
+            return c;
         }
     }
 }
